2bf5c069ee275055f8b1c45f3cf4c72261d7b0e6
Hamdan Romadhona <104075365+hamdanromad@users.noreply.github.com>
2022-06-28 22:40:37 +0700

    Update python101.py

--- python101.py
+++ python101.py
@@ -13,4 +13,20 @@ df_slice = (
          )
         ]
 )
-print("Slice langsung berdasarkan kolom:\n", df_slice)
+print("Slice langsung berdasarkan kolom:\n", df_slice);
+
+"""
+Slicing-indexing
+"""
+import pandas as pd
+# Baca file sample_csv.csv
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/sample_csv.csv")
+# Set index dari df sesuai instruksi
+df = df.set_index(["order_date", 
+                   "order_id", 
+                   "product_id"])
+# Slice sesuai intruksi
+df_slice = df.loc[("2019-01-01",
+                   1612339,
+                   ["P2154","P2159"]),:]
+print("Slice df:\n", df_slice);

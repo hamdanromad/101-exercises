404448e2507f946c7553a7bf5399cad5ff6c7314
Hamdan Romadhona <104075365+hamdanromad@users.noreply.github.com>
2022-06-29 11:00:45 +0700

    Update python101.py

--- python101.py
+++ python101.py
@@ -30,3 +30,130 @@ df_slice = df.loc[("2019-01-01",
                    1612339,
                    ["P2154","P2159"]),:]
 print("Slice df:\n", df_slice);
+
+"""
+Transformation on DF
+"""
+import pandas as pd
+import numpy as pd
+# Baca file sample_csv.csv
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/sample_csv.csv")
+""" 
+1. Change dtypes to datetime """
+# Tampilkan tipe data
+print("Tipe data df:\n", df.dtypes)
+# Ubah tipe data kolom order_date menjadi datetime
+df["order_date"] = pd.to_datetime(df["order_date"])
+# Tampilkan tipe data df setelah transformasi
+print("\nTipe data df setelah transformasi:\n", df.dtypes)
+""" 
+2. Change to float and category """
+# Ubah tipe data kolom quantity menjadi tipe data numerik float
+df["quantity"] = pd.to_numeric(df["quantity"], downcast="float")
+# Ubah tipe data kolom city menjadi tipe data category
+df["city"] = df["city"].astype("category")
+# Tampilkan tipe data df setelah transformasi
+print("\nTipe data df setelah transformasi:\n", df.dtypes)
+""" 
+3. Use .apply and .map (ke satu kolom) """
+# Cetak 5 baris teratas kolom brand
+print("Kolom brand awal:\n", df["brand"].head())
+# Gunakan method apply untuk merubah isi kolom menjadi lower case
+df["brand"] = df["brand"].apply(lambda x: x.lower())
+# Cetak 5 baris teratas kolom brand
+print("Kolom brand setelah apply:\n", df["brand"].head())
+# Gunakan method map untuk mengambil kode brand yaitu karakter terakhirnya
+df["brand"] = df["brand"].map(lambda x: x[-1])
+# Cetak 5 baris teratas kolom brand
+print("Kolom brand setelah map:\n", df["brand"].head())
+"""
+4. Use .applymap (ke DataFrame)
+Buatlah matriks random ukuran 3 x 4 dengan seed random-nya 1234. 
+Kemudian gunakan fungsi kuadrat x**2 + 3*x + 2.
+"""
+# number generator, set angka seed menjadi suatu angka, bisa semua angka, 
+# supaya hasil random nya selalu sama ketika kita run
+np.random.seed(1234)
+# create dataframe 3 baris dan 4 kolom dengan angka random
+df_tr = pd.DataFrame(np.random.rand(3,4)) 
+# Cetak dataframe
+print("Dataframe:\n", df_tr)
+# Cara 1 dengan tanpa define function awalnya, langsung pake fungsi anonymous lambda x
+df_tr1 = df_tr.applymap(lambda x: x**2 + 3*x + 2) 
+print("\nDataframe - cara 1:\n", df_tr1)
+# Cara 2 dengan define function 
+def qudratic_fun(x):
+	return x**2 + 3*x + 2
+df_tr2 = df_tr.applymap(qudratic_fun)
+print("\nDataframe - cara 2:\n", df_tr2)
+print("\nDataframe - cara 2:\n", df_tr2)
+
+"""
+Treatment Missing Value
+"""
+import pandas as pd
+import numpy as np
+# Baca file "https://storage.googleapis.com/dqlab-dataset/datacovid19.csv"
+df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/datacovid19.csv")
+# Lihat dgn .info()
+print(df.info())
+# Lihat MVnya
+print(df.isna().sum())
+"""
+1. delete missing value
+"""
+# Cetak ukuran awal dataframe
+print("Ukuran awal df: %d baris, %d kolom." % df.shape)
+# Drop kolom yang seluruhnya missing value dan cetak ukurannya
+df = df.dropna(axis=1, how="all")
+print("Ukuran df setelah buang kolom dengan seluruh data missing: %d baris, %d kolom." % df.shape)
+# Drop baris jika ada satu saja data yang missing dan cetak ukurannya
+df = df.dropna(axis=0, how="any")
+print("""Ukuran df setelah dibuang baris yang memiliki sekurangnya 1 
+missing value: %d baris, %d kolom.""" % df.shape)
+"""
+2. replace with certain text
+"""
+# Cetak unique value pada kolom province_state
+print("Unique value awal:\n", df["province_state"].unique())
+# Ganti missing value dengan string "unknown_province_state"
+df["province_state"] = df["province_state"].fillna("unknown_province_state")
+# Cetak kembali unique value pada kolom province_state
+print("Unique value setelah fillna:\n", df["province_state"].unique())
+"""
+3. replace with median/mean
+"""
+# Cetak nilai mean dan median awal 
+print("Awal: mean = %f, median = %f." % (df["active"].mean(), df["active"].median()))
+# Isi missing value kolom active dengan median
+df_median = df["active"].fillna(df["active"].median())
+# Cetak nilai mean dan median awal setelah diisi dengan median
+print("Fillna median: mean = %f, median = %f." % (df_median.mean(), df_median.median()))
+# Isi missing value kolom active dengan mean
+df_mean = df["active"].fillna(df["active"].mean())
+# Cetak nilai mean dan median awal setelah diisi dengan mean
+print("Fillna mean: mean = %f, median = %f." % (df_mean.mean(), df_mean.median()))
+"""
+4. use interpolation
+"""
+# Data
+ts = pd.Series({
+   "2020-01-01":9,
+   "2020-01-02":np.nan,
+   "2020-01-05":np.nan,
+   "2020-01-07":24,
+   "2020-01-10":np.nan,
+   "2020-01-12":np.nan,
+   "2020-01-15":33,
+   "2020-01-17":np.nan,
+   "2020-01-16":40,
+   "2020-01-20":45,
+   "2020-01-22":52,
+   "2020-01-25":75,
+   "2020-01-28":np.nan,
+   "2020-01-30":np.nan
+})
+# Isi missing value menggunakan interpolasi linier
+ts = ts.interpolate()
+# Cetak time series setelah interpolasi linier
+print("Setelah diisi missing valuenya:\n", ts)
